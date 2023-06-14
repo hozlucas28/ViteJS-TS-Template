2e43dc91172e7bd07463e83e9aea09f4419d77a8
hozlucas28 <hozlucas28@gmail.com>
2023-06-14 11:23:09 -0300

    Create alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), svgr()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@assets': path.resolve(__dirname, './src/assets'),
+			'@components': path.resolve(__dirname, './src/components'),
+			'@core': path.resolve(__dirname, './src/core'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@services': path.resolve(__dirname, './src/services'),
+			'@utils': path.resolve(__dirname, './src/utils'),
+		},
+	},
 })
